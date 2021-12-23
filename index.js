147020e69d4bddfa59224272917e179216178122
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2021-12-23 10:29:59 +0300

    Done

---
+++ index.js
@@ -0,0 +1,24 @@
+let fadhi = [];
+let hobbies = ["singing","dancing","diving","sports","oud","music"] 
+let numbers = [1,2,3,4,5]
+
+console.log(numbers[1]);
+
+console.log(hobbies[hobbies.length - 1]);
+
+hobbies.push("fake hobby")
+
+console.log(hobbies[hobbies.length - 1]);
+
+console.log(hobbies.length);
+
+hobbies.pop()
+hobbies.pop()
+
+console.log(hobbies.length);
+
+hobbies = []
+
+console.log(hobbies);
+
+
